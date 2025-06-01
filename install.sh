1cb19e799776efa37d28895ae71efe66c025921a
Jonathan Becerra <jonathan.d.becerra@gmail.com>
2025-05-31 21:30:38 -0500

    update: The script should work with macOS and arch-linux
    I don't check againts any other distro, I'm fine with arch.

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-sudo pacman -S --needed - <packages
+# Platform detection
+IS_MACOS=false
+IS_LINUX=false
+
+case "$OSTYPE" in
+darwin*) IS_MACOS=true ;;
+linux*) IS_LINUX=true ;;
+esac
+
+if $IS_LINUX; then
+  sudo pacman -S --needed - <packages
+fi
+
+if $IS_MACOS; then
+  brew install $(<packages)
+fi
